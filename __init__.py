0f816cec2673bdf57b05329f273c586e57ccf8ea
Sara Saleh <sara.sale7@me.com>
2019-01-20 19:27:36 +0300

    File Not Found

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ import requests
 
 app = Flask(__name__)
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r')
+CLIENT_ID = json.loads(open('/var/www/itemcatalog/itemcatalog/client_secrets.json', 'r')
                        .read())['web']['client_id']
 APPLICATION_NAME = "Restaurant Menu Application"
 
